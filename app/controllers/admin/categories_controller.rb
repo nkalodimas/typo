85f7e9aecb4747acf568a58efda20a88f8fa96fe
Nikolaos Kalodimas <nkalodimas@gmail.com>
2013-11-22 09:32:23 -0800

    fixed categories bug

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -25,9 +25,9 @@ class Admin::CategoriesController < Admin::BaseController
 
   def new_or_edit
     @categories = Category.find(:all)
-    @category = Category.find(params[:id])
-    @category.attributes = params[:category]
     if request.post?
+      @category = Category.find(params[:id])
+      @category.attributes = params[:category]
       respond_to do |format|
         format.html { save_category }
         format.js do 
